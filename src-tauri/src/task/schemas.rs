70f545f0ce8a2b5abb88cbace121000cfc4fc1ee
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-10-26 23:01:25 +0900

    add task structs

---
+++ src-tauri/src/task/schemas.rs
@@ -0,0 +1,23 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+pub struct Task {
+    id: i32,
+    title: String,
+    started_at: String,
+    ended_at: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateTask {
+    pub title: String,
+    pub started_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UpdateTask {
+    pub title: Option<String>,
+    pub started_at: Option<String>,
+    pub ended_at: Option<String>,
+}
