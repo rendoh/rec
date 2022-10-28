e158212a28d5755ed96b0730d7bcfd9a9b76e43e
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-10-29 08:14:28 +0900

    add vanilla extreact css

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,5 +22,6 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'warn',
   },
 };
