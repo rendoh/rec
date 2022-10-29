56fb83368a12b59f23088ea9378ed3c59af89269
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-10-29 12:22:10 +0900

    use icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import 'normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
-// import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
