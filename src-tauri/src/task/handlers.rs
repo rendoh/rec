97f24cbc4eb167a15dc5ae52ad4c7615cb61c220
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-11-03 15:30:38 +0900

    use indoc

--- src-tauri/src/task/handlers.rs
+++ src-tauri/src/task/handlers.rs
@@ -1,6 +1,7 @@
 use crate::error::ApiError;
 
 use super::schemas::{CreateTask, FindTasks, Task, UpdateTask};
+use indoc::indoc;
 use sqlx::{
     sqlite::{SqlitePool, SqliteRow},
     Row,
@@ -8,20 +9,19 @@ use sqlx::{
 use validator::Validate;
 
 pub async fn find_all(pool: &SqlitePool, payload: FindTasks) -> Result<Vec<Task>, ApiError> {
-    sqlx::query_as::<_, Task>(
-        r#"
-SELECT
-    tasks.id,
-    tasks.title,
-    strftime('%Y-%m-%dT%H:%M:%fZ', tasks.started_at, 'utc') AS started_at,
-    strftime('%Y-%m-%dT%H:%M:%fZ', tasks.ended_at, 'utc') AS ended_at
-FROM
-    tasks
-WHERE
-    tasks.started_at BETWEEN $1 AND $2
-ORDER BY
-    tasks.started_at DESC"#,
-    )
+    sqlx::query_as::<_, Task>(indoc! {r#"
+        SELECT
+            tasks.id,
+            tasks.title,
+            strftime('%Y-%m-%dT%H:%M:%fZ', tasks.started_at, 'utc') AS started_at,
+            strftime('%Y-%m-%dT%H:%M:%fZ', tasks.ended_at, 'utc') AS ended_at
+        FROM
+            tasks
+        WHERE
+            tasks.started_at BETWEEN $1 AND $2
+        ORDER BY
+            tasks.started_at DESC
+    "#})
     .bind(
         payload
             .from
@@ -36,18 +36,17 @@ ORDER BY
 pub async fn create(pool: &SqlitePool, payload: CreateTask) -> Result<Task, ApiError> {
     payload.validate().map_err(ApiError::ValidationError)?;
 
-    let active_task = sqlx::query_as::<_, Task>(
-        r#"
-SELECT
-    tasks.id,
-    tasks.title,
-    strftime('%Y-%m-%dT%H:%M:%fZ', tasks.started_at, 'utc') AS started_at,
-    strftime('%Y-%m-%dT%H:%M:%fZ', tasks.ended_at, 'utc') AS ended_at
-FROM
-    tasks
-WHERE
-    ended_at IS NULL"#,
-    )
+    let active_task = sqlx::query_as::<_, Task>(indoc! {r#"
+        SELECT
+            tasks.id,
+            tasks.title,
+            strftime('%Y-%m-%dT%H:%M:%fZ', tasks.started_at, 'utc') AS started_at,
+            strftime('%Y-%m-%dT%H:%M:%fZ', tasks.ended_at, 'utc') AS ended_at
+        FROM
+            tasks
+        WHERE
+            ended_at IS NULL
+    "#})
     .fetch_optional(pool)
     .await
     .map_err(ApiError::DbError)?;
@@ -62,32 +61,30 @@ WHERE
         .map_err(ApiError::ValidationError)?;
     }
 
-    sqlx::query(
-        r#"
-UPDATE
-    tasks
-SET
-    ended_at = datetime($1, 'localtime')
-WHERE
-    ended_at IS NULL"#,
-    )
+    sqlx::query(indoc! {r#"
+        UPDATE
+            tasks
+        SET
+            ended_at = datetime($1, 'localtime')
+        WHERE
+            ended_at IS NULL
+    "#})
     .bind(&payload.started_at)
     .execute(pool)
     .await
     .map_err(ApiError::DbError)?;
 
-    sqlx::query_as::<_, Task>(
-        r#"
-INSERT INTO
-    tasks (title, started_at)
-VALUES
-    ($1, datetime($2, 'localtime'))
-RETURNING
-    id,
-    title,
-    strftime('%Y-%m-%dT%H:%M:%fZ', started_at, 'utc') AS started_at,
-    strftime('%Y-%m-%dT%H:%M:%fZ', ended_at, 'utc') AS ended_at"#,
-    )
+    sqlx::query_as::<_, Task>(indoc! {r#"
+        INSERT INTO
+            tasks (title, started_at)
+        VALUES
+            ($1, datetime($2, 'localtime'))
+        RETURNING
+            id,
+            title,
+            strftime('%Y-%m-%dT%H:%M:%fZ', started_at, 'utc') AS started_at,
+            strftime('%Y-%m-%dT%H:%M:%fZ', ended_at, 'utc') AS ended_at
+    "#})
     .bind(payload.title.trim())
     .bind(&payload.started_at)
     .fetch_one(pool)
@@ -98,31 +95,30 @@ RETURNING
 pub async fn update(pool: &SqlitePool, id: i32, payload: UpdateTask) -> Result<Task, ApiError> {
     payload.validate().map_err(ApiError::ValidationError)?;
 
-    sqlx::query_as::<_, Task>(
-        r#"
-UPDATE
-    tasks
-SET
-    title = ifnull(
-        $2,
-        title
-    ),
-    started_at = ifnull(
-        datetime($3, 'localtime'),
-        started_at
-    ),
-    ended_at = ifnull(
-        datetime($4, 'localtime'),
-        ended_at
-    )
-WHERE
-    id = $1
-RETURNING
-    id,
-    title,
-    strftime('%Y-%m-%dT%H:%M:%fZ', started_at, 'utc') AS started_at,
-    strftime('%Y-%m-%dT%H:%M:%fZ', ended_at, 'utc') AS ended_at"#,
-    )
+    sqlx::query_as::<_, Task>(indoc! {r#"
+        UPDATE
+            tasks
+        SET
+            title = ifnull(
+                $2,
+                title
+            ),
+            started_at = ifnull(
+                datetime($3, 'localtime'),
+                started_at
+            ),
+            ended_at = ifnull(
+                datetime($4, 'localtime'),
+                ended_at
+            )
+        WHERE
+            id = $1
+        RETURNING
+            id,
+            title,
+            strftime('%Y-%m-%dT%H:%M:%fZ', started_at, 'utc') AS started_at,
+            strftime('%Y-%m-%dT%H:%M:%fZ', ended_at, 'utc') AS ended_at
+    "#})
     .bind(id)
     .bind(payload.title.map(|title| title.trim().to_string()))
     .bind(payload.started_at)
@@ -133,13 +129,12 @@ RETURNING
 }
 
 pub async fn delete(pool: &SqlitePool, id: i32) -> Result<(), ApiError> {
-    sqlx::query(
-        r#"
-DELETE FROM
-    tasks
-WHERE
-    id = $1"#,
-    )
+    sqlx::query(indoc! {r#"
+        DELETE FROM
+            tasks
+        WHERE
+            id = $1
+    "#})
     .bind(id)
     .execute(pool)
     .await
@@ -149,20 +144,19 @@ WHERE
 }
 
 pub async fn find_recent_tasks(pool: &SqlitePool) -> Result<Vec<String>, ApiError> {
-    let rows = sqlx::query(
-        r#"
-SELECT
-    title,
-    Max(started_at)
-FROM
-    tasks
-GROUP BY
-    title
-ORDER BY
-    started_at DESC
-LIMIT
-    6"#,
-    )
+    let rows = sqlx::query(indoc! {r#"
+        SELECT
+            title,
+            Max(started_at)
+        FROM
+            tasks
+        GROUP BY
+            title
+        ORDER BY
+            started_at DESC
+        LIMIT
+            6
+        "#})
     .map(|row: SqliteRow| row.try_get("title").unwrap())
     .fetch_all(pool)
     .await
