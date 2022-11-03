0bad458b51e6b3072fd482e60a5de08323f2c599
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-11-03 15:20:54 +0900

    collect error messages to use simple error schema

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,5 +1,5 @@
 use serde::Serialize;
-use validator::ValidationErrors;
+use validator::{ValidationErrors, ValidationErrorsKind};
 
 #[derive(Debug, thiserror::Error)]
 pub enum ApiError {
@@ -15,23 +15,98 @@ impl Serialize for ApiError {
         S: serde::Serializer,
     {
         match self {
-            Self::ValidationError(validation_errors) => validation_errors.serialize(serializer),
-            Self::DbError(sqlx_error) => DbError::new(sqlx_error.to_string()).serialize(serializer),
+            Self::ValidationError(validation_errors) => {
+                Error::new(collect_validation_messages(validation_errors)).serialize(serializer)
+            }
+            Self::DbError(sqlx_error) => {
+                Error::new(vec![sqlx_error.to_string()]).serialize(serializer)
+            }
         }
     }
 }
 
-#[derive(Debug, Serialize)]
-struct DbError {
-    message: String,
-    code: String,
+fn collect_validation_messages(validation_errors: &ValidationErrors) -> Vec<String> {
+    validation_errors
+        .errors()
+        .clone()
+        .into_values()
+        .flat_map(|kind| match kind {
+            ValidationErrorsKind::Field(errors) => errors
+                .iter()
+                .map(|error| {
+                    if let Some(val) = &error.message {
+                        val.to_string()
+                    } else {
+                        "".to_string()
+                    }
+                })
+                .collect::<Vec<String>>(),
+            ValidationErrorsKind::Struct(errors) => collect_validation_messages(&errors),
+            ValidationErrorsKind::List(errors) => errors
+                .into_values()
+                .flat_map(|error| collect_validation_messages(&error))
+                .collect::<Vec<String>>(),
+        })
+        .collect::<Vec<String>>()
 }
 
-impl DbError {
-    pub fn new(message: String) -> Self {
-        Self {
-            message,
-            code: "dberror".to_string(),
-        }
+#[derive(Debug, Serialize)]
+struct Error {
+    messages: Vec<String>,
+}
+
+impl Error {
+    pub fn new(messages: Vec<String>) -> Self {
+        Self { messages }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use serde::Deserialize;
+    use validator::Validate;
+
+    use super::collect_validation_messages;
+
+    #[derive(Debug, Validate, Deserialize)]
+    struct SignupData {
+        #[validate]
+        contact_details: ContactDetails,
+        #[validate]
+        preferences: Vec<Preference>,
+        #[validate(range(min = 18, max = 20, message = "range"))]
+        age: u32,
+    }
+
+    #[derive(Debug, Validate, Deserialize)]
+    struct ContactDetails {
+        #[validate(email(message = "email"))]
+        mail: String,
+    }
+
+    #[derive(Debug, Validate, Deserialize)]
+    struct Preference {
+        #[validate(length(min = 4, message = "min"))]
+        name: String,
+    }
+
+    #[test]
+    fn test_collect_errors() {
+        let signup_data = SignupData {
+            contact_details: ContactDetails {
+                mail: "".to_string(),
+            },
+            preferences: vec![
+                Preference {
+                    name: "".to_string(),
+                },
+                Preference {
+                    name: "".to_string(),
+                },
+            ],
+            age: 1,
+        };
+        let messages = collect_validation_messages(&signup_data.validate().unwrap_err());
+        assert!(messages.len() == 4);
     }
 }
