8af06d165b544f718c9599d9733f5f4ece2ddcf4
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-11-04 21:38:01 +0900

    use app dir

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -5,8 +5,38 @@ use sqlx::{
     sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions, SqliteSynchronous},
     SqlitePool,
 };
+use tauri::AppHandle;
 
-pub async fn create_pool(database_url: &str) -> Result<SqlitePool, Box<dyn std::error::Error>> {
+pub async fn setup_db(app_handle: AppHandle) -> Result<SqlitePool, sqlx::Error> {
+    const DATABASE_FILE: &str = "database.db";
+    let app_dir = app_handle
+        .path_resolver()
+        .app_dir()
+        .expect("cannot get app dir");
+    let database_file = app_dir.join(DATABASE_FILE);
+    let db_exists = std::fs::metadata(&database_file).is_ok();
+
+    if !db_exists {
+        std::fs::create_dir(&app_dir).expect("cannot create db directory");
+    }
+
+    let database_dir_str = std::fs::canonicalize(&app_dir)
+        .unwrap()
+        .to_string_lossy()
+        .replace('\\', "/");
+    let database_url = format!("sqlite://{}/{}", database_dir_str, DATABASE_FILE);
+    let sqlite_pool = create_pool(&database_url)
+        .await
+        .expect("cannot connect to db");
+
+    if !db_exists {
+        migrate(&sqlite_pool).await?
+    }
+
+    Ok(sqlite_pool)
+}
+
+async fn create_pool(database_url: &str) -> Result<SqlitePool, Box<dyn std::error::Error>> {
     let connection_options = SqliteConnectOptions::from_str(database_url)?
         .create_if_missing(true)
         .journal_mode(SqliteJournalMode::Wal)
@@ -19,6 +49,6 @@ pub async fn create_pool(database_url: &str) -> Result<SqlitePool, Box<dyn std::
     Ok(sqlite_pool)
 }
 
-pub async fn migrate(pool: &SqlitePool) -> Result<(), MigrateError> {
+async fn migrate(pool: &SqlitePool) -> Result<(), MigrateError> {
     sqlx::migrate!("./migrations").run(pool).await
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,31 +10,14 @@ use tauri::Manager;
 
 fn main() {
     use tauri::async_runtime::block_on;
-    const DATABASE_DIR: &str = ".rec_app_db";
-    const DATABASE_FILE: &str = "db.sqlite";
-    let home_dir = directories::UserDirs::new()
-        .map(|dirs| dirs.home_dir().to_path_buf())
-        .expect("cannot access home directory");
-    let database_dir = home_dir.join(DATABASE_DIR);
-    let database_file = database_dir.join(DATABASE_FILE);
-    let db_exists = std::fs::metadata(&database_file).is_ok();
-
-    if !db_exists {
-        std::fs::create_dir(&database_dir).expect("cannot create db directory");
-    }
-
-    let database_dir_str = std::fs::canonicalize(&database_dir)
-        .unwrap()
-        .to_string_lossy()
-        .replace('\\', "/");
-    let database_url = format!("sqlite://{}/{}", database_dir_str, DATABASE_FILE);
-    let sqlite_pool = block_on(db::create_pool(&database_url)).expect("cannot connect to db");
-
-    if !db_exists {
-        block_on(db::migrate(&sqlite_pool)).expect("failed to migrate db");
-    }
 
     tauri::Builder::default()
+        .setup(|app| {
+            let sqlite_pool =
+                block_on(db::setup_db(app.app_handle())).expect("cannot create database");
+            app.manage(sqlite_pool);
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             task::commands::find_all,
             task::commands::create,
@@ -42,10 +25,6 @@ fn main() {
             task::commands::delete,
             task::commands::find_recent_tasks,
         ])
-        .setup(|app| {
-            app.manage(sqlite_pool);
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
